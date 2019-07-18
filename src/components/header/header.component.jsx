0a9f78fea178dc24abb3b7be24dad3c9da7b6c81
CasinoRoyal <lux_ex_tenebris@outlook.com>
2019-07-18 10:52:02 +0300

    Added signIn-signOut btn in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => { 
+const Header = ({ currentUser }) => { 
+  console.log(currentUser)
   return(
     <div className="header">
       <div className="logo-container">
@@ -14,13 +16,25 @@ const Header = () => {
         </Link>
       </div>
       <div className="options">
-        <Link  className="option" to='/shop'>
+        <Link className="option" to='/shop'>
           Shop
         </Link>
 
-        <Link  className="option" to='/contact'>
+        <Link className="option" to='/contact'>
           Contact
         </Link>
+
+        {
+          currentUser ?
+            <button className="option" onClick={() => auth.signOut()}>
+              Sign out
+            </button>
+            :
+            <Link className="option" to='/login'>
+              Sign in
+            </Link>
+        }
+
       </div>
     </div>
   )
